3e2c7b78d5e3fd786ab29430dbd868bfb277fa94
tsabitahinayah <tsabitahinayah1403@mail.ugm.ac.id>
2025-03-25 20:19:25 -1100

    Menambah user dan product routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const sequelize = require('./config/database');
+
+const userRoutes = require('./routes/UserRoutes');
+const productRoutes = require('./routes/ProductRoutes');
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+
+sequelize.authenticate()
+    .then(() => console.log('PostgreSQL connected'))
+    .catch(err => console.error('Database connection error:', err));
 
 // Default Route
 app.get('/', (req, res) => {
