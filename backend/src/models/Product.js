36d1902c133825b5932a74eb3584230518e1ac15
bulanmurela <bulanaputrim@gmail.com>
2025-03-27 22:51:28 +0700

    Update atribut & add new models

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,18 +1,58 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const User = require('./User');
+const Category = require('./Category');
+const Image = require('./Image');
 
 const Product = sequelize.define('Product', {
-    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-    name: { type: DataTypes.STRING, allowNull: false },
-    stock: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
-    userId: { type: DataTypes.UUID, allowNull: false }
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true, 
+        primaryKey: true 
+    },
+    name: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    stock: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false, 
+        defaultValue: 0 
+    },
+    unit_price: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+    cogs_per_unit: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+    category_id: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+    image_id: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+    username: { 
+        type: DataTypes.STRING, 
+        allowNull: false, 
+        references: { 
+            model: User, 
+            key: 'username' 
+        }, 
+            onUpdate: 'CASCADE', 
+            onDelete: 'CASCADE' 
+    }
 }, {
     timestamps: true,
     tableName: 'products'
 });
 
-// Relasi ke User
-Product.belongsTo(User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+// Relasi ke tabel lain
+Product.belongsTo(Category, { foreignKey: 'category_id', onDelete: 'SET NULL' });
+Product.belongsTo(Image, { foreignKey: 'image_id', onDelete: 'SET NULL' });
+Product.belongsTo(User, { foreignKey: 'username', targetKey: 'username', onDelete: 'CASCADE' });
 
 module.exports = Product;
